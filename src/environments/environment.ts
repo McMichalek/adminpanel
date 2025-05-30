bf13e128af87d71ac57f7bfc2968581fb7263809
McMichalek <nexik.official@gmail.com>
2025-05-30 11:25:41 +0200

    Not even work
    Tried to set up login via firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAumij-8vlOEouFgKJMKTnz9--ex93ABDk",
+    authDomain: "agh-fried-chicken.firebaseapp.com",
+    databaseURL: "https://agh-fried-chicken-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "agh-fried-chicken",
+    storageBucket: "agh-fried-chicken.firebasestorage.app",
+    messagingSenderId: "1023748219674",
+    appId: "1:1023748219674:web:d16a865ad64f9c75e702a3",
+    measurementId: "G-TD1T2EKYST"
+  }
+};
